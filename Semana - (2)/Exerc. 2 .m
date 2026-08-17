7f1918f948a786771b37b93f7b95f19b36c0cc9c
BPrestes-Engineer <prestesbruno378@gmail.com>
2026-08-16 22:58:41 -0300

    Exercício 2
    Calcula o custo energético de um laboratório de informática.

--- Semana - (2)/Exerc. 2 .m
+++ Semana - (2)/Exerc. 2 .m
@@ -4,15 +4,13 @@
 #Objetivo: Calcular o Custo energético de um laboratório de informática, podendo
 #          simular outros cenários também.
 
-#-------------------------------------------------------------------------------
 clear;
 clc;
-
 #-------------------------------------------------------------------------------
 #Declaração-De-Variáveis
 
-numeroMaquinas = 0;
-horasUsoDiario = 0;
+numeroMaquinas = input(' Quantos Computadores estão sendo utilizados?:  ');
+horasUsoDiario = input(' Quantas horas, em média, cada computador é utilizado diariamente?:  ');
 consumoMedio = 0.35; #De w(Watzs) para Kw(KiloWatzs)
 diasMes = 22;
 tarifa = 0.92; #Por KwH
@@ -25,12 +23,6 @@ custoAnual = 0;
 
 #-------------------------------------------------------------------------------
 #Processamento-De-Dados
-
-printf('\n')
-numeroMaquinas = input(' Quantos Computadores estão sendo utilizados?:  ');
-
-printf('\n')
-horasUsoDiario = input(' Quantas horas, em média, cada computador é utilizado diariamente?:  ');
 
   consumoMensal = numeroMaquinas * consumoMedio * horasUsoDiario * diasMes;
 
@@ -49,13 +41,13 @@ printf('\n\n-------------------------------------------------------------\n\n\n'
 
 printf(' O Consumo Mensal é igual a %.2f KwH.\n\n', consumoMensal);
 
-printf(' O Custo Mensal sem Tributação é igual a %.2f KwH.\n\n', custoMensal);
+printf(' O Custo Mensal sem Tributação é igual a R$%.2f.\n\n', custoMensal);
 
-printf(' O Custo Mensal com Tributação é igual a %.2f KwH.\n\n', custoMensalTributo);
+printf(' O Custo Mensal com Tributação é igual a R$%.2f.\n\n', custoMensalTributo);
 
-printf(' O Custo Anual é igual a %.2f KwH.\n\n', custoAnual);
+printf(' O Custo Anual é igual a R$%.2f.\n\n', custoAnual);
 
-printf(' O Custo por Maquina é igual a %.2f KwH.\n\n', custoMaquina);
+printf(' O Custo por Maquina é igual a R$%.2f.\n\n', custoMaquina);
 
 printf('\n\n-------------------------------------------------------------\n\n\n');
 
@@ -63,7 +55,14 @@ printf(' Format Short:\n ');
 consumoMensal
 
 printf('\n\n Format Long:\n ');
-format long;
+format long
 consumoMensal
 
-format short;
+#{
+Ao observar os dois formatos, é notório que o format short possui menos casas
+decimais do que o format long. Logo, conclui-se que essa diferença seria relevante
+em cenários onde a precisão matemática é obrigatória, como na projeção de uma
+aeronave, por exemplo.
+#}
+
+format short
